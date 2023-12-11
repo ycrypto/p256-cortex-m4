7671b8251e63021022fe19e2a5306c3160eea01a
Sosthène Guédon <sosthene@nitrokey.com>
2023-12-11 16:36:11 +0100

    Update cryptographic dependencies

--- src/fallback.rs
+++ src/fallback.rs
@@ -1,6 +1,6 @@
 use core::convert::TryInto;
 
-use ecdsa::hazmat::{SignPrimitive, VerifyPrimitive};
+use ecdsa::signature::hazmat::{PrehashVerifier, RandomizedPrehashSigner};
 use elliptic_curve::sec1::ToEncodedPoint;
 use rand_core::{CryptoRng, RngCore};
 
@@ -46,7 +46,7 @@ impl Keypair {
     /// Generate a random `Keypair`.
     ///
     /// The implementation uses rejection sampling.
-    pub fn random(rng: impl CryptoRng + RngCore) -> Self {
+    pub fn random<R: CryptoRng + RngCore>(rng: &mut R) -> Self {
         let secret = SecretKey(p256::SecretKey::random(rng));
         let public = secret.public_key();
 
@@ -58,13 +58,13 @@ impl SecretKey {
     /// Generate a random `SecretKey`.
     ///
     /// The implementation uses rejection sampling.
-    pub fn random(rng: impl CryptoRng + RngCore) -> Self {
+    pub fn random<R: CryptoRng + RngCore>(rng: &mut R) -> Self {
         SecretKey(p256::SecretKey::random(rng))
     }
 
     /// Verifies that there are 32 bytes that correspond to a big-endian integer in the range 1..=n-1.
     pub fn from_bytes(bytes: impl AsRef<[u8]>) -> Result<Self> {
-        Ok(SecretKey(p256::SecretKey::from_bytes(bytes)?))
+        Ok(SecretKey(p256::SecretKey::from_slice(bytes.as_ref())?))
     }
 
     /// Return secret scalar as big-endian integer.
@@ -83,23 +83,15 @@ impl SecretKey {
     pub fn sign_prehashed(
         &self,
         prehashed_message: &[u8],
-        rng: impl CryptoRng + RngCore,
+        mut rng: impl CryptoRng + RngCore,
     ) -> Signature {
-        let prehashed_message_as_scalar =
-            p256::Scalar::from_bytes_reduced(prehashed_message.try_into().unwrap());
-        let mut rng = rng;
-        let static_scalar = p256::Scalar::from_bytes_reduced(&self.0.to_bytes());
-        loop {
-            let ephemeral_secret = p256::SecretKey::random(&mut rng);
-            let ephemeral_scalar = p256::Scalar::from_bytes_reduced(&ephemeral_secret.to_bytes());
-            let blinded_scalar = p256::BlindedScalar::new(ephemeral_scalar, &mut rng);
+        let signing_key: p256::ecdsa::SigningKey = (&self.0).into();
 
-            if let Ok(signature) =
-                static_scalar.try_sign_prehashed(&blinded_scalar, &prehashed_message_as_scalar)
-            {
-                return Signature(signature);
-            }
-        }
+        Signature(
+            signing_key
+                .sign_prehash_with_rng(&mut rng, prehashed_message)
+                .unwrap(),
+        )
     }
 
     #[cfg(feature = "prehash")]
@@ -115,7 +107,7 @@ impl SecretKey {
     /// ECDH key agreement.
     pub fn agree(&self, other: &PublicKey) -> SharedSecret {
         SharedSecret(elliptic_curve::ecdh::diffie_hellman(
-            self.0.to_secret_scalar(),
+            self.0.to_nonzero_scalar(),
             other.0.as_affine(),
         ))
     }
@@ -179,11 +171,9 @@ impl PublicKey {
     /// Verify signature on message assumed to be hashed, if needed.
     #[must_use = "The return value indicates if the message is authentic"]
     pub fn verify_prehashed(&self, prehashed_message: &[u8], signature: &Signature) -> bool {
-        let prehashed_message_as_scalar =
-            p256::Scalar::from_bytes_reduced(prehashed_message.try_into().unwrap());
-        self.0
-            .as_affine()
-            .verify_prehashed(&prehashed_message_as_scalar, &signature.0)
+        let verifying_key: p256::ecdsa::VerifyingKey = self.0.into();
+        verifying_key
+            .verify_prehash(prehashed_message, &signature.0)
             .is_ok()
     }
 
@@ -224,7 +214,7 @@ impl Signature {
 
     /// Encode signature from big-endian r, then big-endian s, without framing.
     pub fn to_untagged_bytes(&self) -> [u8; 64] {
-        self.0.as_ref().try_into().unwrap()
+        self.0.to_bytes().into()
     }
 
     /// Encode signature as SEC1 ASN.1 DER
@@ -243,6 +233,6 @@ impl Signature {
 impl SharedSecret {
     /// The secret (big-endian x-coordinate)
     pub fn as_bytes(&self) -> &[u8; 32] {
-        self.0.as_bytes().as_ref()
+        self.0.raw_secret_bytes().as_ref()
     }
 }
